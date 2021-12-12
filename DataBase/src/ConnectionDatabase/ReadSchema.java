785a890a3c5ca58afe954aa4136048e7ab45f749
OMAR <68195919+v5ln@users.noreply.github.com>
2021-12-12 18:26:37 +0200

    Point No. 2 : Managing DB
    - Create interface for managing the DB
    - Create Factory Design class to managing ( ReadSchema , InsertData , Read Table DeleteTable ) Classes
    - Create and finish ReadSchema Class

---
+++ DataBase/src/ConnectionDatabase/ReadSchema.java
@@ -0,0 +1,25 @@
+package ConnectionDatabase;
+
+import java.sql.Connection;
+import java.sql.*;
+
+public class ReadSchema implements ManagingDatabase {
+    public void getSchema(Connection connection){
+        try {
+            // getting Database Schema Names
+            ResultSet rs = connection.getMetaData().getCatalogs();
+            while (rs.next()) {
+                System.out.println("Schema Name - " + rs.getString("TABLE_CAT"));
+            }
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void managingtable(Connection databaseConnection) {
+        getSchema(databaseConnection);
+
+    }
+}
---
+++ DataBase/src/ConnectionDatabase/ReadTable.java
@@ -0,0 +1,12 @@
+package ConnectionDatabase;
+
+import java.sql.Connection;
+
+public class ReadTable implements ManagingDatabase{
+
+
+    @Override
+    public void managingtable(Connection databaseConnection) {
+
+    }
+}
