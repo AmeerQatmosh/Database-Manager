f3694f5a63d00bc4c33ba8ac17d48ead67f25309
zaidmelhem <68277240+zaidmelhem@users.noreply.github.com>
2021-12-13 21:32:42 +0200

    Update mysql.java

--- DataBase/src/profile/mysql.java
+++ DataBase/src/profile/mysql.java
@@ -1,5 +1,17 @@
 package profile;
-
+import java.sql.*;
 public class mysql {
+	public Connection getconnectio() {
+	try{  
+		 
+		Connection con=DriverManager.getConnection(  
+		"jdbc:mysql://localhost:3306/mydb","root","qwe123!@#");  
+		Statement stmt=con.createStatement();  
+		
+		return con ;
+		}catch(Exception e){ System.out.println(e);}
+	return null ;  
+		}
 
 }
+
