00ff16732d92f9cd9c6295c9a5879b845b89c42c
zaidmelhem <zaidomar2011426@gmail.com>
2021-12-14 13:44:07 +0200

    finish export
    Co-Authored-By: lmaaljohare <68349552+lmaaljohare@users.noreply.github.com>

--- DataBase/src/export/Export.java
+++ DataBase/src/export/Export.java
@@ -7,46 +7,33 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.sql.*;
 import java.util.Scanner;
+import java.util.Date;
+import com.opencsv.CSVWriter;
 
 public class Export extends SchemaName {
 
     public static void exportData(Connection connection){
-//        String filename =
-//                "C:\\Documents and Settings\\admin\\My Documents\\NetBeansProjects\\AutomateExcelDatabase\\myjdbcfile.csv";
         try {
-
-//            CSVWriter writer = new CSVWriter(new FileWriter("yourfile.csv"), '\t');
-//            Boolean includeHeaders = true;
-//
-//            java.sql.ResultSet myResultSet = .... //your resultset logic here
-//
-//            writer.writeAll(myResultSet, includeHeaders);
-//
-//            writer.close();
-
-        	 PrintWriter pw= new PrintWriter(new File("C:\\Users\\MSI\\Desktop\\test2\\test3.csv"));
-        	    StringBuilder sb=new StringBuilder();
 
             Scanner sc= new Scanner(System.in);
             System.out.println("Enter Table Name");
             String table = sc.nextLine();
 
-
-
+            Date date = new Date(); 
+            String name = "C:\\Users\\MSI\\Desktop\\test2\\"+table+" "+date.toString().replace(' ','-').replace(':', ';')+".csv";
+            CSVWriter writer = new CSVWriter(new FileWriter(name, false),',',
+            CSVWriter.NO_QUOTE_CHARACTER,
+            CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+            CSVWriter.DEFAULT_LINE_END);
+            
+            Boolean includeHeaders = true;
             Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM "+getGetName()+"."+table);
-            int columnCount = rs.getMetaData().getColumnCount();
-            while (rs.next()) {
-                for (int i =1;i<=columnCount;i++){
-                    sb.append(rs.getString(i));
-                    sb.append(",");
-                }
-                sb.append("\r\n");
-            }
+            ResultSet myResultSet = stmt.executeQuery("SELECT * FROM "+getGetName()+"."+table);
 
-            pw.write(sb.toString());
-            pw.close();
-            System.out.println("finished");
+            writer.writeAll(myResultSet, includeHeaders);
+            
+            writer.close();
+            
             System.out.println("CSV File is created successfully.");
         } catch (Exception e) {
             e.printStackTrace();
