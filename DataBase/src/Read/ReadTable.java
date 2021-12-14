dae628dc2ef76af35c73c76bc088f486876f0c2e
OMAR <68195919+v5ln@users.noreply.github.com>
2021-12-14 11:38:48 +0200

    Update ReadTable.java
    Co-Authored-By: lmaaljohare <68349552+lmaaljohare@users.noreply.github.com>

--- DataBase/src/Read/ReadTable.java
+++ DataBase/src/Read/ReadTable.java
@@ -11,25 +11,30 @@ public class ReadTable extends SchemaName implements ManagingDatabase {
     public void getTable(Connection connection){
         try {
             int count = 0;
+            // Show all tables from selected schema
+            DatabaseMetaData md = connection.getMetaData();
+            ResultSet rs1 = md.getTables(null, getGetName(), "%", null);
+            while (rs1.next()) {
+                System.out.println(rs1.getString(3));
+            }
+
+            //Get table name from user
             Scanner sc= new Scanner(System.in);
             System.out.println("Enter Table Name");
             String table = sc.nextLine();
-            DatabaseMetaData md = connection.getMetaData();
-            ResultSet rs1 = md.getTables(null, getGetName(), table, null);
-            while (rs1.next()) {
-                System.out.println(rs1.getString(3));
-                count++;
-            }
 
-
+            //Show all columns names for selected table
             Statement stmt = connection.createStatement();
             ResultSet rs2 = stmt.executeQuery("SELECT * FROM "+getGetName()+"."+table);
-
+            ResultSetMetaData rsmd = rs2.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            for (int i =1;i<=columnCount;i++){
+                System.out.print(rsmd.getColumnName(i)+"\t");
+            }
             while (rs2.next()) {
-                for (int i =0;i<count;i++){
+                for (int i =1;i<=columnCount;i++){
                     System.out.print(rs2.getString(i)+"\t");
                 }
-
             }
         } catch(SQLException e) {
             System.out.println("SQL exception occured" + e);
